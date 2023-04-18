fe3ccd2585059ae4db5558c864bfb46734f111fb
luca betti sorbelli <luca betti sorbelli>
2023-04-18 15:34:00 +0200

    ciclo for

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
-console.log ("hello world")
+let bottone = document.getElementById('bottone');
+
+bottone.addEventListener('click', function() {
+    
+    let emailValide = ["lucabetti@gmail.com", "marcorossi@gmail.com", "mariobianchi@gmail.com", "francoverdi@gmail.com"];
+    
+    let email = document.getElementById('email').value;
+
+    for (let i = 0; i < emailValide.length; i++) {
+
+        const emailCorrente = emailValide[i]
+        
+        if (emailValide.includes(email)) {
+            console.log('Email valida');
+        } else {
+            console.log('Email errata');
+        }
+        console.log(emailCorrente);
+    } 
+});
+
+
+
