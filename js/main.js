92468a84f1877e36279796d1924cf666fb419518
luca betti sorbelli <luca betti sorbelli>
2023-04-18 15:37:19 +0200

    if else

--- js/main.js
+++ js/main.js
@@ -1,23 +1,34 @@
 let bottone = document.getElementById('bottone');
 
+// bottone.addEventListener('click', function() {
+    
+//     let emailValide = ["lucabetti@gmail.com", "marcorossi@gmail.com", "mariobianchi@gmail.com", "francoverdi@gmail.com"];
+    
+//     let email = document.getElementById('email').value;
+
+//     for (let i = 0; i < emailValide.length; i++) {
+
+//         const emailCorrente = emailValide[i]
+        
+//         if (emailValide.includes(email)) {
+//             console.log('Email valida');
+//         } else {
+//             console.log('Email errata');
+//         }
+//         console.log(emailCorrente);
+//     } 
+// });
+
+
 bottone.addEventListener('click', function() {
-    
     let emailValide = ["lucabetti@gmail.com", "marcorossi@gmail.com", "mariobianchi@gmail.com", "francoverdi@gmail.com"];
-    
     let email = document.getElementById('email').value;
 
-    for (let i = 0; i < emailValide.length; i++) {
-
-        const emailCorrente = emailValide[i]
-        
-        if (emailValide.includes(email)) {
-            console.log('Email valida');
-        } else {
-            console.log('Email errata');
-        }
-        console.log(emailCorrente);
-    } 
+    if (emailValide.includes(email)) {
+        console.log('Email valida');
+    } else {
+        console.log('Email errata');
+    }
 });
-
 
 
